3130a0efe73f80a011eb1bcbb3e4b6b2e3b0a7d5
Jhon_Parada <paradajhon221@gmail.com>
2026-07-14 15:00:50 -0500

    Add files via upload

--- PASEOFELIZ/model/conexion.php
+++ PASEOFELIZ/model/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$host = "sql113.byethost17.com";   // MySQL hostname
-$user = "b17_42313426";           // MySQL username
-$pass = "1092941961";      // clave real de VistaPanel
-$db   = "b17_42313426_paseofeliztest"; // El nombre EXACTO de tu base de datos
+$host = "oculto";   // MySQL hostname
+$user = "oculto";           // MySQL username
+$pass = "oculto";      // clave real de VistaPanel
+$db   = "oculto"; // El nombre EXACTO de tu base de datos
 
 $conn = new mysqli($host, $user, $pass, $db);
 
